515c5f8044ab660f65b601fd8e32b228b957ef54
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-24 16:48:30 +0000

    refactor for loops and fizz buzz game

--- 03_Control_Flow/02_For_Loop.py
+++ 03_Control_Flow/02_For_Loop.py
@@ -31,22 +31,3 @@ i = 0
 
 # for index, letter in enumerate(my_list):
 #     print(f"{letter} is in position {index}")
-
-# user input for min and max number
-min_number = int(input("Please choose a minimum number"))
-max_number = int(input("Please choose a maximum number"))
-
-# user input for alternate words
-fizz_alt_word = input("Choose an alternative word for 'Fizz'")
-buzz_alt_word = input("Choose an alternative word for 'Buzz'")
-
-
-for number in range(min_number, max_number):
-    if (number % 5) == 0 and (number % 3) == 0:
-        print(fizz_alt_word + buzz_alt_word)
-    elif (number % 3) == 0:
-        print(fizz_alt_word)
-    elif (number % 5) == 0:
-        print(buzz_alt_word)
-    else:
-        print(number)
--- 03_Control_Flow/Fizz_Buzz.py
+++ 03_Control_Flow/Fizz_Buzz.py
@@ -1,13 +1,13 @@
 # user input for min and max number
-min_number = int(input("Please choose a minimum number"))
-max_number = int(input("Please choose a maximum number"))
+min_number = int(input("Please choose a minimum number\n"))
+max_number = int(input("Please choose a maximum number\n"))
 
 # user input for alternate words
-fizz_alt_word = input("Choose an alternative word for 'Fizz'")
-buzz_alt_word = input("Choose an alternative word for 'Buzz'")
+fizz_alt_word = input("Choose an alternate word for 'Fizz'\n")
+buzz_alt_word = input("Choose an alternate word for 'Buzz'\n")
 
 # checks every number in range if divisible with 3, 5 and prints the appropriate value
-for number in range(min_number, max_number):
+for number in range(min_number, max_number + 1):
     if (number % 5) == 0 and (number % 3) == 0:
         print(fizz_alt_word + buzz_alt_word)
     elif (number % 3) == 0:
