ca69f51d7deb9ef9e29a28c0700f78363f5060ca
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-24 16:28:19 +0000

    add for loops

--- 03_Control_Flow/02_For_Loop.py
+++ 03_Control_Flow/02_For_Loop.py
@@ -17,7 +17,7 @@ i = 0
 #     i = i + 1
 #     print(i)
 
-me = {"name": "Tudor", "age": 375, "job": "DevOps Engineer"}
+# me = {"name": "Tudor", "age": 375, "job": "DevOps Engineer"}
 
 # for thing in me:
 #     print(thing)  # prints every key
@@ -29,5 +29,24 @@ me = {"name": "Tudor", "age": 375, "job": "DevOps Engineer"}
 # for key, value in me.items():
 #     print(f"My {key} is {value}")
 
-for index, letter in enumerate(my_list):
-    print(f"{letter} is in position {index}")
+# for index, letter in enumerate(my_list):
+#     print(f"{letter} is in position {index}")
+
+# user input for min and max number
+min_number = int(input("Please choose a minimum number"))
+max_number = int(input("Please choose a maximum number"))
+
+# user input for alternate words
+fizz_alt_word = input("Choose an alternative word for 'Fizz'")
+buzz_alt_word = input("Choose an alternative word for 'Buzz'")
+
+
+for number in range(min_number, max_number):
+    if (number % 5) == 0 and (number % 3) == 0:
+        print(fizz_alt_word + buzz_alt_word)
+    elif (number % 3) == 0:
+        print(fizz_alt_word)
+    elif (number % 5) == 0:
+        print(buzz_alt_word)
+    else:
+        print(number)
