b062d1f81fcbe0ec9a3cf548ec3161668ede2f98
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-24 17:56:40 +0000

    add error handling to fizzbuzz game

--- 03_Control_Flow/Fizz_Buzz.py
+++ 03_Control_Flow/Fizz_Buzz.py
@@ -1,18 +1,21 @@
 # user input for min and max number
-min_number = int(input("Please choose a minimum number\n"))
-max_number = int(input("Please choose a maximum number\n"))
+min_number = input("Please choose a minimum number\n")
+max_number = input("Please choose a maximum number\n")
 
 # user input for alternate words
 fizz_alt_word = input("Choose an alternate word for 'Fizz'\n")
 buzz_alt_word = input("Choose an alternate word for 'Buzz'\n")
 
-# checks every number in range if divisible with 3, 5 and prints the appropriate value
-for number in range(min_number, max_number + 1):
-    if (number % 5) == 0 and (number % 3) == 0:
-        print(fizz_alt_word + buzz_alt_word)
-    elif (number % 3) == 0:
-        print(fizz_alt_word)
-    elif (number % 5) == 0:
-        print(buzz_alt_word)
-    else:
-        print(number)
+if min_number.isnumeric() and max_number.isnumeric():
+    # checks every number in range if divisible with 3, 5 and prints the appropriate value
+    for number in range(int(min_number), int(max_number) + 1):
+        if (number % 5) == 0 and (number % 3) == 0:
+            print(fizz_alt_word + buzz_alt_word)
+        elif (number % 3) == 0:
+            print(fizz_alt_word)
+        elif (number % 5) == 0:
+            print(buzz_alt_word)
+        else:
+            print(number)
+else:
+    print("Opps! Please choose a number!")
