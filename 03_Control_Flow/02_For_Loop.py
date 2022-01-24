ffe377ea29476ea23e697d4e814df3881314056d
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-24 16:04:14 +0000

    add for loops

---
+++ 03_Control_Flow/02_For_Loop.py
@@ -0,0 +1,33 @@
+# For loops
+
+my_list = ["a", "b", "c"]
+i = 0
+
+# for letter in my_list:
+#     if letter == "b":
+#         print(letter.upper())
+#     else:
+#         print(letter)
+
+
+# for i in range(1, 10):
+#     for letter in my_list:
+#         print(i, letter)
+#
+#     i = i + 1
+#     print(i)
+
+me = {"name": "Tudor", "age": 375, "job": "DevOps Engineer"}
+
+# for thing in me:
+#     print(thing)  # prints every key
+#     print(me[thing])  # prints the value for every key
+
+# for thing in me.values():
+#     print(thing)
+
+# for key, value in me.items():
+#     print(f"My {key} is {value}")
+
+for index, letter in enumerate(my_list):
+    print(f"{letter} is in position {index}")
