54ded71e8c8305ad14b05f8e8ed58ed9b8aef4ad
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-24 10:49:45 +0000

    add intro to variables

---
+++ 01_variables/01_variables_intro.py
@@ -0,0 +1,12 @@
+# Python is a dynamically typed language
+
+a = 1  # int (Integer)
+b = 2  # Int
+c = 3.5  # Float (floating-point number)
+d = "Hello"  # String
+
+result = str((a + c) * b)
+print(d)   # calling the "print" function
+
+print(result, type(result))  # printing multiple values on the same line by separating them by a comma ","
+
