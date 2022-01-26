aed9beb2084aa85181fd5cbe8a5619c59e43c370
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-26 10:49:30 +0000

    update classes intro

--- 05_Classes/01_Class_intro.py
+++ 05_Classes/01_Class_intro.py
@@ -22,19 +22,25 @@ class Dog:
         return self.bark().upper()
 
 
-fido = Dog()
-print(fido, type(fido))
-
-print(fido.animal_kind)
-print(fido.bark())
-print(fido.bark_again(3))
-print(fido.who_am_i())
-print(fido.loud_bark())
+# fido = Dog()
+# print(fido, type(fido))
+#
+# print(fido.animal_kind)
+# print(fido.bark())
+# print(fido.bark_again(3))
+# print(fido.who_am_i())
+# print(fido.loud_bark())
 
 
 class Cat:
-
-    animal_kind = "Feline"
+    # when using this way of creating a class, the class itself won't have for example here,
+    # an animal kind BUT, every INSTANCE created WILL HAVE an animal kind
+    def __init__(self, breed, age, color):  # Initialisation, dunder (double underscore) init
+        self.animal_kind = "Feline"
+        self.legs = 4
+        self.breed = breed
+        self.age = age
+        self.color = color
 
     def meow(self):
         return "Meow!"
@@ -49,20 +55,29 @@ class Cat:
         return "Meow! " * number_of_meows
 
 
-garfield = Cat()
-
-print(garfield.meow())
-print(garfield.loud_meow())
-print(garfield.who_am_i())
-print(garfield.meow_repeatedly(6))
-
-# instantiation - i.e. creating an INSTANCE of a class
-tom = Cat()
-schumi = Cat()
-
-print(schumi.animal_kind)
-Cat.animal_kind = "FELINE"
-print(schumi.animal_kind)
+garfield = Cat("Persian", 4, "orange")
+schumi = Cat("Sphynx", 9, "blue")
 
 
+class Car:
+    # gearbox has default value
+    def __init__(self, make, model, engine_size, color, transmission, gearbox="5 speed manual"):
+        self.make = make
+        self.model = model
+        self.engine_size = engine_size
+        self.color = color
+        self.transmission = transmission
+        self.gearbox = gearbox
 
+    # decorator mo make pycharm happy with the method being in a class
+    @staticmethod
+    def rev_engine(self):
+        return "Vrrruummm"
+
+    @staticmethod
+    def loud_rev(self):
+        return "Vvvvrrrruuuuummmmm"
+
+
+first_car = Car("Toyota", "Aristo", 3000, "black", "RWD", "6 speed automatic")
+print(first_car.make, first_car.model, first_car.engine_size, first_car.color, first_car.transmission, first_car.gearbox)
