cdfecef05785c9efa28efd32d3b72ab7359d4eb0
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-27 10:19:40 +0000

    add definitions and examples to pillars of oop

--- 05_Classes/03_Pillars_OOP.py
+++ 05_Classes/03_Pillars_OOP.py
@@ -1,9 +1,19 @@
 # Pillars of OOP
 
 # Abstraction
+# We use Abstraction for hiding the internal details or implementations of a function and showing its functionalities
+# only.
+# Example: This is similar to the way you know how to drive a car without knowing the background mechanism.
 
 # Encapsulation
+# Encapsulation, is a way to ensure security. Basically, it hides the data from the access of outsiders.
 
 # Inheritance
+# Inheritance is the procedure in which one class inherits the attributes and methods of another class.
+# Its like we compare an older model of a car with a new model. The new model inherits the traits of the previous model
+# and also has a few new features
 
 # Polymorphism
+# So Polymorphism means having many forms. In OOP it refers to the functions having the same names but
+# carrying different functionalities.
+# Different engines in the same car
