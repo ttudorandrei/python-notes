5c204c2e461e731a8f86a9147aaa47881c65191c
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-27 13:51:14 +0000

    add example extra test case for divide

--- unit_testing/test_pytest_simplecalc.py
+++ unit_testing/test_pytest_simplecalc.py
@@ -1,6 +1,6 @@
 # PyTest
 from unit_testing.simplecalc import SimpleCalc
-
+import pytest
 
 def test_calc_add():
     calc = SimpleCalc()
@@ -14,9 +14,20 @@ def test_calc_subtract():
 
 def test_calc_multiply():
     calc = SimpleCalc()
-    assert calc.multiply(3, 6) == 18
+    # assert calc.multiply(3, 6) == 18
+    # result of multiplication would be -32.3600000000001 and not a perfect match to -32.36
+    # so we use the approx method to approximate the result
+    assert pytest.approx(calc.multiply(0.4, -80.9)) == -32.36
 
 
 def test_calc_divide():
     calc = SimpleCalc()
     assert calc.divide(6, 3) == 2
+
+
+# this will use a context manager (keyword: with)
+def test_calc_divide_by_zero_raises_error():
+    # context manager = with
+    with pytest.raises(ZeroDivisionError):
+        calc = SimpleCalc()
+        calc.divide(1, 0)
